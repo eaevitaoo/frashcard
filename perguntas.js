f707a93db4ed2968952fc4cd655b0d7749b2ae1b
eaevitaoo <vitor.rodrigues23@escola.pr.gov.br>
2024-10-02 14:35:14 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -17,7 +17,36 @@ criaCartao(
 )
 
 criaCartao(
-    'GitHub',
-    'Como acessar o GitHub?',
-    'Com login e senha.'
+    'Escola',
+    'Qual é a importância da diversidade nas escolas?',
+    'A diversidade enriquece o ambiente escolar, promove empatia e prepara os alunos para um mundo multicultural.'
 )
+criaCartao(
+    'Escola',
+    'Quais são os principais desafios enfrentados pelos alunos hoje?',
+    'Os alunos enfrentam pressão acadêmica, bullying, ansiedade e problemas de saúde mental.'
+)
+criaCartao(
+    'Escola',
+    'Como os pais podem apoiar a educação dos filhos?',
+    'Os pais podem ajudar com os deveres, incentivar a leitura e se envolver em atividades escolares.'
+)
+criaCartao(
+    'Escola',
+    'Qual é a importância da educação na formação de um indivíduo?',
+    'A educação ajuda a desenvolver habilidades, pensamento crítico e prepara os indivíduos para o mercado de trabalho..'
+)
+criaCartao(
+    'Meio Ambiente',
+    'Por que a preservação do meio ambiente é importante?',
+    'A preservação do meio ambiente é crucial para manter a biodiversidade, garantir recursos naturais e proteger a saúde humana.'
+)
+criaCartao(
+    'Meio Ambiente',
+    'Quais são as principais causas do desmatamento?',
+    'As principais causas incluem agricultura extensiva, exploração madeireira, urbanização e incêndios florestais.'
+)
+criaCartao(
+    'Meio Ambiente',
+    'Qual é o papel da educação ambiental?',
+    'A educação ambiental informa e sensibiliza as pessoas sobre questões ambientais, promovendo práticas sustentáveis e responsabilidade coletiva.'
