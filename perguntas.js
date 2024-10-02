f62461664aa6f6a90bcb14e437b7879cd25a53b5
eaevitaoo <vitor.rodrigues23@escola.pr.gov.br>
2024-10-02 14:49:44 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,52 +1,62 @@
-criaCartao(
+criaCartao
+(
     'Portugues',
     'No alfabeto, depois do A, vem que letra?',
     'Resposta: B.'
 )
 
-criaCartao(
+criaCartao
+(
     'O que o vitao anda pensando.',
     'Onde o capeta foi considerado menino?',
     'Na casa do wevirty.'
 )
 
-criaCartao(
+criaCartao
+(
     'Matematica',
     'Quanto é 5 + 5?',
     'Respota: 10.'
 )
 
-criaCartao(
+criaCartao
+(
     'Escola',
     'Qual é a importância da diversidade nas escolas?',
     'A diversidade enriquece o ambiente escolar, promove empatia e prepara os alunos para um mundo multicultural.'
 )
-criaCartao(
+criaCartao
+(
     'Escola',
     'Quais são os principais desafios enfrentados pelos alunos hoje?',
     'Os alunos enfrentam pressão acadêmica, bullying, ansiedade e problemas de saúde mental.'
 )
-criaCartao(
+criaCartao
+(
     'Escola',
     'Como os pais podem apoiar a educação dos filhos?',
     'Os pais podem ajudar com os deveres, incentivar a leitura e se envolver em atividades escolares.'
 )
-criaCartao(
+criaCartao
+(
     'Escola',
     'Qual é a importância da educação na formação de um indivíduo?',
     'A educação ajuda a desenvolver habilidades, pensamento crítico e prepara os indivíduos para o mercado de trabalho..'
 )
-criaCartao(
+criaCartao
+(
     'Meio Ambiente',
     'Por que a preservação do meio ambiente é importante?',
     'A preservação do meio ambiente é crucial para manter a biodiversidade, garantir recursos naturais e proteger a saúde humana.'
 )
-criaCartao(
+criaCartao
+(
     'Meio Ambiente',
     'Quais são as principais causas do desmatamento?',
     'As principais causas incluem agricultura extensiva, exploração madeireira, urbanização e incêndios florestais.'
 )
-criaCartao(
+criaCartao
+(
     'Meio Ambiente',
     'Qual é o papel da educação ambiental?',
     'A educação ambiental informa e sensibiliza as pessoas sobre questões ambientais, promovendo práticas sustentáveis e responsabilidade coletiva.'
